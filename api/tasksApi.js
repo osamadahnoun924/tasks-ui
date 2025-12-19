fcd1ad4823a94f1ceff16a7a40665ce1daae221a
Osama Dahnoun <odahnoun@cisco.com>
2025-12-19 08:09:04 -0500

    remove secrets from code

--- api/tasksApi.js
+++ api/tasksApi.js
@@ -11,20 +11,17 @@ export const tasksApi = {
     return request("/api/tasks", {
       method: "POST",
       body: JSON.stringify(body),
-      headers: { "x-api-key": "supersecret123" },
     });
   },
   update(id, body) {
     return request(`/api/tasks/${id}`, {
       method: "PATCH",
       body: JSON.stringify(body),
-      headers: { "x-api-key": "supersecret123" },
     });
   },
   delete(id) {
     return request(`/api/tasks/${id}`, {
       method: "DELETE",
-      headers: { "x-api-key": "supersecret123" },
     });
   },
 };
